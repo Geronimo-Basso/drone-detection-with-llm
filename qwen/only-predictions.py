4d5c8d90d923409ffff3e70b593ab79663446fcc
Geronimo <gerobasso01@gmail.com>
2024-06-14 22:26:38 +0200

    Added dataset

--- qwen/only-predictions.py
+++ qwen/only-predictions.py
@@ -1,7 +1,4 @@
 import os
-import re
-from pathlib import Path
-from PIL import Image, ImageDraw
 import torch
 from transformers import AutoModelForCausalLM, AutoTokenizer
 from transformers.generation import GenerationConfig
@@ -32,44 +29,6 @@ tokenizer = AutoTokenizer.from_pretrained("Qwen/Qwen-VL-Chat", trust_remote_code
 model = AutoModelForCausalLM.from_pretrained("Qwen/Qwen-VL-Chat", device_map="cuda", trust_remote_code=True).eval()
 model.generation_config = GenerationConfig.from_pretrained("Qwen/Qwen-VL-Chat", trust_remote_code=True)
 
-
-# Function to normalize and format bounding boxes
-def normalize_and_format_bounding_boxes(matches, image_width, image_height):
-    normalized_boxes = []
-    for i in range(0, len(matches), 2):
-        x1, y1 = map(int, matches[i].strip('()').split(','))
-        x2, y2 = map(int, matches[i + 1].strip('()').split(','))
-
-        center_x = (x1 + x2) / 2 / image_width
-        center_y = (y1 + y2) / 2 / image_height
-        width = (x2 - x1) / image_width
-        height = (y2 - y1) / image_height
-
-        normalized_boxes.append(f"{center_x:.10f} {center_y:.10f} {width:.10f} {height:.10f}")
-    return normalized_boxes
-
-
-# Function to draw bounding boxes
-def draw_bounding_boxes(image, matches):
-    draw = ImageDraw.Draw(image)
-    for i in range(0, len(matches), 2):
-        x1, y1 = map(int, matches[i].strip('()').split(','))
-        x2, y2 = map(int, matches[i + 1].strip('()').split(','))
-        draw.rectangle([x1, y1, x2, y2], outline="red", width=2)
-
-
-# Function to parse bounding boxes from text files
-def parse_bounding_boxes(file_path):
-    with open(file_path, 'r') as file:
-        lines = file.readlines()
-    bounding_boxes = []
-    for line in lines:
-        if line.strip() != "0 0 0 0":
-            coords = list(map(float, line.strip().split()[1:]))  # Ignore the leading '0'
-            bounding_boxes.append(coords)
-    return bounding_boxes
-
-
 # Processing images
 image_prediction = 0
 image_no_prediction = 0
@@ -90,4 +49,4 @@ for file_name in images_filenames:
     ])
     response, history = model.chat(tokenizer, query=query, history=None)
 
-    # print(response)
+    print(response)
